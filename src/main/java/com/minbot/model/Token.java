413dfb10bf26628db06a30e7f9c5369de0c8845b
jsong00505 <jsong00505@gmail.com>
2018-04-03 19:07:50 -0700

    refactor: add Token model

---
+++ src/main/java/com/minbot/model/Token.java
@@ -0,0 +1,24 @@
+package com.minbot.model;
+
+public class Token {
+
+  private String botName;
+
+  private String botToken;
+
+  public String getBotName() {
+    return botName;
+  }
+
+  public void setBotName(String botName) {
+    this.botName = botName;
+  }
+
+  public String getBotToken() {
+    return botToken;
+  }
+
+  public void setBotToken(String botToken) {
+    this.botToken = botToken;
+  }
+}
